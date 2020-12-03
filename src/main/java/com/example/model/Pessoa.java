37f982dcbc1b443ce257350108f3cded3fdc6241
leonardos <leonardos@koerich.com.br>
2020-12-03 10:37:07 -0300

    add validacoes em Pessoa e criado PessoaRepository

--- src/main/java/com/example/model/Pessoa.java
+++ src/main/java/com/example/model/Pessoa.java
@@ -3,12 +3,13 @@ package com.example.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,11 +21,25 @@ public class Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     private String nome;
+
     private String sexo;
+
+    @Email
     private String email;
+
+    @Column(name="data_nascimento")
     private Date dataNascimento;
+
     private String naturalidade;
     private String nacionalidade;
+
+    @Column(unique=true)
     private String cpf;
+
+    @Column(name = "atualizado", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "dd/MM/YYYY")
+    public Timestamp atualizado;
 }
